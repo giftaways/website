10fc35f559d0c542f8c5115937b5169523dc707f
Gersti <patrick.gerstacker@gmail.com>
2023-09-22 21:14:09 +0200

    feat: add prettier

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,13 +1,11 @@
 import { Footer } from "@/components/footer";
 import { ReactNode } from "react";
 
-export default function DefaultLayout( { children } : { children: ReactNode }){
-    return(          
-    <div className='min-h-screen flex flex-col bg-gray-950'>
-      <div className='flex grow'>
-        {children}
-      </div>
-      <Footer/>
-  </div>
-  )
+export default function DefaultLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex min-h-screen flex-col bg-gray-950">
+      <div className="flex grow">{children}</div>
+      <Footer />
+    </div>
+  );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,22 +1,23 @@
-import { GitHubLogoIcon, DiscordLogoIcon } from "@radix-ui/react-icons"
-import Link from "next/link"
+import { GitHubLogoIcon, DiscordLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export const Footer = () => {
-
-    return(
-        <div className="w-full py-5 flex justify-center text-gray-400 text-sm bg-gray-900">
-            <div className="max-w-screen-2xl flex-col flex text-center items-center w-full justify-between md:flex-row gap-3 mx-3">
-                <p className="w-[245px]">©{new Date().getFullYear()} Botnatic - All rights reserved.</p>
-                <div className="flex gap-6 items-center">
-                    <Link href="/privacy-policy">Privacy Policy</Link>
-                    <Link href="/terms-of-service">Terms of Service</Link>
-                    <Link href="/imprint">Imprint</Link>
-                </div>
-                <div className="w-[245px] flex gap-6 text-lg justify-center md:justify-end">
-                    <GitHubLogoIcon className="scale-150 cursor-pointer"/>
-                    <DiscordLogoIcon className="scale-150 cursor-pointer"/>
-                </div>
-            </div>
+  return (
+    <div className="flex w-full justify-center bg-gray-900 py-5 text-sm text-gray-400">
+      <div className="mx-3 flex w-full max-w-screen-2xl flex-col items-center justify-between gap-3 text-center md:flex-row">
+        <p className="w-[245px]">
+          ©{new Date().getFullYear()} Botnatic - All rights reserved.
+        </p>
+        <div className="flex items-center gap-6">
+          <Link href="/privacy-policy">Privacy Policy</Link>
+          <Link href="/terms-of-service">Terms of Service</Link>
+          <Link href="/imprint">Imprint</Link>
         </div>
-    )
-}
+        <div className="flex w-[245px] justify-center gap-6 text-lg md:justify-end">
+          <GitHubLogoIcon className="scale-150 cursor-pointer" />
+          <DiscordLogoIcon className="scale-150 cursor-pointer" />
+        </div>
+      </div>
+    </div>
+  );
+};
