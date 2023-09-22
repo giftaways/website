755edea77a64ee3bb24932820c172d2db2f6d63d
Gersti <patrick.gerstacker@gmail.com>
2023-09-22 21:54:23 +0200

    feat: add navbar

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,9 +1,11 @@
 import { Footer } from "@/components/footer";
+import { Navbar } from "@/components/navbar";
 import { ReactNode } from "react";
 
 export default function DefaultLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col bg-gray-950">
+      <Navbar />
       <div className="flex grow">{children}</div>
       <Footer />
     </div>
---
+++ components/navbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { DiscordLogoIcon } from "@radix-ui/react-icons";
+
+export const Navbar = () => {
+  return (
+    <div className="flex w-full justify-center py-5">
+      <div className="mx-3 flex w-full max-w-screen-2xl items-center justify-between gap-3 text-center">
+        <div className="flex items-center gap-3">
+          <Image src="/logo.png" width={35} height={35} alt="giftaways" />
+          <p className="text-lg font-bold">Giftaways</p>
+        </div>
+        <div className="flex gap-6">
+          <Button className="hidden gap-3 sm:flex" variant="ghost">
+            Support
+          </Button>
+          <Button className="flex gap-3">
+            <DiscordLogoIcon className="scale-150" />
+            Add to Discord
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
