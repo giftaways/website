662aea293c1d8bcefa8a4ea67ee9e711f7e5195b
Gersti <patrick.gerstacker@gmail.com>
2023-09-25 21:28:13 +0200

    fix: logo as link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
 
@@ -5,10 +6,12 @@ export const Navbar = () => {
   return (
     <div className="flex w-full justify-center py-5">
       <div className="mx-3 flex w-full max-w-screen-2xl items-center justify-between gap-3 text-center">
-        <div className="flex items-center gap-3">
-          <Image src="/logo.png" width={35} height={35} alt="giftaways" />
-          <p className="text-lg font-bold">Giftaways</p>
-        </div>
+        <Link href="/">
+          <div className="flex items-center gap-3">
+            <Image src="/logo.png" width={35} height={35} alt="giftaways" />
+            <p className="text-lg font-bold">Giftaways</p>
+          </div>
+        </Link>
         <div className="flex gap-6">
           <Button className="flex gap-3">Support Server</Button>
         </div>
